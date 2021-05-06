c1c17f5a465b74ae4c430451f0761a6d08bf6e02
Fenofanik <Barakamonoleg@gmail.com>
2021-05-06 16:10:32 +0300

    Test1

---
+++ lib/widgets/MainScreenPages/ProductsData.dart
@@ -0,0 +1,6 @@
+class Data {
+  String image, brand, material, size, length, width, uploadId;
+
+  Data(this.image, this.brand, this.material, this.size, this.length,
+      this.width, this.uploadId);
+}
--- lib/widgets/MainScreenPages/data.dart
+++
@@ -1,6 +0,0 @@
-class Data {
-  String imgUrl, brand, material, size, length, width, uploadId;
-
-  Data(this.imgUrl, this.brand, this.material, this.size, this.length,
-      this.width, this.uploadId);
-}
