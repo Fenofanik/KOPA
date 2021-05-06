8a064e85cf50a903ea3ee403edf5a564d6c9ead2
Fenofanik <Barakamonoleg@gmail.com>
2021-05-06 12:19:33 +0300

    KOPA

---
+++ lib/widgets/MainScreenPages/data.dart
@@ -0,0 +1,6 @@
+class Data {
+  String imgUrl, brand, material, size, length, width, uploadId;
+
+  Data(this.imgUrl, this.brand, this.material, this.size, this.length,
+      this.width, this.uploadId);
+}
